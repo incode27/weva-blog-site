f23f58579b20f1dcf5b829f445d0b8c6031a743f
misbachul27 <misbachulmunr@gmail.com>
2022-06-22 13:14:11 +0700

    initial commit

---
+++ components/MobileCall.js
@@ -0,0 +1,26 @@
+
+
+export default function MobileCall() {
+    return (
+        <>
+            <div className="bg-black grid grid-cols-5 gap-0 place-items-center h-20 text-sm" >
+                <div className="text-white">
+                    01
+                </div>
+                <div className="text-white ">
+                    02
+                </div>
+                <div className="text-white">
+                    03
+                </div>
+                <div className="text-white">
+                    04
+                </div>
+                <div className="text-white">
+                    05
+                </div>
+
+            </div>
+        </>
+    )
+}
